987f228184f36e2dff5fa07bf0ddafaebc93a026
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2019-12-05 21:53:36 -0800

    added a view high score page and made elemnts to appear and disapear

--- code.js
+++ code.js
@@ -2,11 +2,21 @@
 var main = document.getElementById("Main-menu");
 var start = document.querySelector("#bt");
 var q_sec= document.querySelector("#question-sec")
+var score=document.querySelector("#score")
 
 //uses the an event listener on the start button to to bring text present
 start.addEventListener("click",function(){
     main.setAttribute("style","display:none")
     q_sec.setAttribute("style","display:contents")
+    creation()    
+})
+score.addEventListener("click",function(event){
+    event.preventDefault()
+    main.setAttribute("style","display:none")
+    q_sec.setAttribute("style","display:none")
+    sp.setAttribute("style","display:contents")
+     //start timer
+
+    //render the question.js
     creation()
-    
 })
